3e1123c234e8ad66ce7543e1e46e1b0508627baa
Vladislav <kiryukhinv@gmail.com>
2015-11-01 22:47:19 +0300

    First version

---
+++ app/src/main/java/pw/kvld/calculator/OnNumClickListener.java
@@ -0,0 +1,15 @@
+package pw.kvld.calculator;
+
+import android.view.View;
+
+public class OnNumClickListener implements View.OnClickListener {
+
+    int number;
+
+    public OnNumClickListener(int num) {
+        number = num;
+    }
+
+    @Override
+    public void onClick(View v) { }
+}
