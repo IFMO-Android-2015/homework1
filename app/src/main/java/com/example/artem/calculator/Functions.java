6ff913906a7645000d4661c5a5913f4751bf7da2
Artem Ohanjanyan <artemohanjanyan@gmail.com>
2015-11-03 16:06:48 +0300

    finished

--- app/src/main/java/com/example/artem/calculator/Functions.java
+++ app/src/main/java/com/example/artem/calculator/Functions.java
@@ -2,53 +2,43 @@ package com.example.artem.calculator;
 
 import java.math.BigDecimal;
 
-/**
- * Created by artem on 10/27/15.
- */
 public class Functions {
     public interface Function {
-        BigDecimal call(BigDecimal a, BigDecimal b);
+        BigDecimal execute(BigDecimal a, BigDecimal b);
     }
 
     public static class FlipConst implements Function {
         @Override
-        public BigDecimal call(BigDecimal a, BigDecimal b) {
+        public BigDecimal execute(BigDecimal a, BigDecimal b) {
             return b;
         }
     }
 
     public static class Add implements Function {
         @Override
-        public BigDecimal call(BigDecimal a, BigDecimal b) {
+        public BigDecimal execute(BigDecimal a, BigDecimal b) {
             return a.add(b);
         }
     }
 
     public static class Subtract implements Function {
         @Override
-        public BigDecimal call(BigDecimal a, BigDecimal b) {
+        public BigDecimal execute(BigDecimal a, BigDecimal b) {
             return a.subtract(b);
         }
     }
 
     public static class Multiply implements Function {
         @Override
-        public BigDecimal call(BigDecimal a, BigDecimal b) {
+        public BigDecimal execute(BigDecimal a, BigDecimal b) {
             return a.multiply(b);
         }
     }
 
     public static class Divide implements Function {
         @Override
-        public BigDecimal call(BigDecimal a, BigDecimal b) {
-            return a.divide(b, BigDecimal.ROUND_CEILING);
-        }
-    }
-
-    public static class Modulo implements Function {
-        @Override
-        public BigDecimal call(BigDecimal a, BigDecimal b) {
-            return a.remainder(b);
+        public BigDecimal execute(BigDecimal a, BigDecimal b) {
+            return a.divide(b, 20, BigDecimal.ROUND_CEILING);
         }
     }
 }
