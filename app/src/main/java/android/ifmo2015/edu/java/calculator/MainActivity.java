7f7eded819c5d1e1687f32a63cbf93f355b3e3c4
utikeev <utikeev@gmail.com>
2015-11-19 17:59:58 +0300

    Added empty project

---
+++ app/src/main/java/android/ifmo2015/edu/java/calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package android.ifmo2015.edu.java.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
