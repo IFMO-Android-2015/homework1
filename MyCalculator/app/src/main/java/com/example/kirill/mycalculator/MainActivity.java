b76f3b232aeec7fc5b1091c458ea70b94e8148f3
kirkutirev <kutirev-k@mail.ru>
2015-12-08 15:28:44 +0300

    done

--- MyCalculator/app/src/main/java/com/example/kirill/mycalculator/MainActivity.java
+++ MyCalculator/app/src/main/java/com/example/kirill/mycalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         MULTIPLICATION ,
         ADDITION,
         DIVISION,
-        SUBSTRACTION
+        SUBTRACTION
     }
 
     private EditText editText;
@@ -28,39 +29,133 @@ public class MainActivity extends AppCompatActivity {
         editText.setEnabled(false);
     }
 
-    private void toDuble(double operand) {
+    private double toDouble() {
         String numb = editText.getText().toString();
+        double operand = 0;
         if(numb.length() > 0) {
             operand = Double.parseDouble(editText.getText().toString());
         }
+        editText.setText("");
+        return operand;
     }
 
+    // worst programming below
     private void add() {
         if(curOperation == Operation.NOTHING) {
+            try {
+                first = toDouble();
+                curOperation = Operation.ADDITION;
+            } catch (Throwable exp) {
+                first = 0;
+                curOperation = Operation.NOTHING;
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Wrong number!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } else if(curOperation != Operation.ADDITION) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Addition now", Toast.LENGTH_SHORT);
+            toast.show();
             curOperation = Operation.ADDITION;
         }
     }
 
-    private void substract() {
+    private void subtract() {
         if(curOperation == Operation.NOTHING) {
-            curOperation = Operation.SUBSTRACTION;
+            try {
+                first = toDouble();
+                curOperation = Operation.SUBTRACTION;
+            } catch (Throwable exp) {
+                first = 0;
+                curOperation = Operation.NOTHING;
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Wrong number!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } else if(curOperation != Operation.SUBTRACTION) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Subtraction now", Toast.LENGTH_SHORT);
+            toast.show();
+            curOperation = Operation.SUBTRACTION;
         }
+
     }
 
-    private void multiplicate() {
+    private void multiply() {
         if(curOperation == Operation.NOTHING) {
+            try {
+                first = toDouble();
+                curOperation = Operation.MULTIPLICATION;
+            } catch (Throwable exp) {
+                first = 0;
+                curOperation = Operation.NOTHING;
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Wrong number!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } else if(curOperation != Operation.MULTIPLICATION) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Multiplication now", Toast.LENGTH_SHORT);
+            toast.show();
             curOperation = Operation.MULTIPLICATION;
         }
     }
 
     private void divide() {
         if(curOperation == Operation.NOTHING) {
+            try {
+                first = toDouble();
+                curOperation = Operation.DIVISION;
+            } catch (Throwable exp) {
+                first = 0;
+                curOperation = Operation.NOTHING;
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Wrong number!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } else if(curOperation != Operation.DIVISION) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Division now", Toast.LENGTH_SHORT);
+            toast.show();
             curOperation = Operation.DIVISION;
         }
     }
 
-    private void culculation() {
+    private void calculation() {
+        if(curOperation != Operation.NOTHING) {
+            try {
+                second = toDouble();
+            } catch (Error exp) {
+                second = 0;
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Wrong number!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
 
+            if (curOperation == Operation.ADDITION) {
+                first += second;
+            } else if (curOperation == Operation.SUBTRACTION) {
+                first -= second;
+            } else if (curOperation == Operation.MULTIPLICATION) {
+                first *= second;
+            } else if (curOperation == Operation.DIVISION) {
+                if (second == 0.0) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Division by zero", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    first /= second;
+                }
+            }
+
+            editText.setText(String.valueOf(first));
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Nothing happened", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        curOperation = Operation.NOTHING;
     }
 
     private void deleting() {
@@ -84,10 +179,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button8: editText.append("8"); break;
             case R.id.button9: editText.append("9"); break;
             case R.id.button10: add(); break;
-            case R.id.button11: substract(); break;
-            case R.id.button12: multiplicate(); break;
+            case R.id.button11: subtract(); break;
+            case R.id.button12: multiply(); break;
             case R.id.button13: divide(); break;
-            case R.id.button14: culculation(); break;
+            case R.id.button14: calculation(); break;
             case R.id.button15: deleting(); break;
         }
     }
