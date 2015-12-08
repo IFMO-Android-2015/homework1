83657299320128ce78136d085d0bed8afd319ec1
qurbonzoda <a.qurbonzoda@gmail.com>
2015-12-09 02:01:17 +0300

    updated

--- SimpleCalculator/app/src/main/java/com/example/qurbonzoda/simplecalculator/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.example.qurbonzoda.simplecalculator;
-
-/**
- * Created by qurbonzoda on 16.10.15.
- */
-public interface Calculator {
-    double calculate(String expression);
-}
--- SimpleCalculator/app/src/main/java/com/example/qurbonzoda/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/example/qurbonzoda/simplecalculator/MainActivity.java
@@ -3,27 +3,23 @@ package com.example.qurbonzoda.simplecalculator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String POW = "^";
-    private static final String CANNOT_CALCULATE = "Cannot calculate";
-    private EditText screen;
-    StringBuilder expression;
+    private EditText num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        expression = new StringBuilder();
-        screen = (EditText) findViewById(R.id.screen);
-        int[] idList = {R.id.button_add, R.id.button_sub, R.id.button_mul, R.id.button_div, R.id.button_pow,
-                R.id.button_mod, R.id.button_zero, R.id.button_one, R.id.button_two, R.id.button_three,
-                R.id.button_four, R.id.button_five, R.id.button_six, R.id.button_seven, R.id.button_eight,
-                R.id.button_nine, R.id.button_equals, R.id.button_dot, R.id.button_del, R.id.button_clear
-        };
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+        int[] idList = {R.id.plus, R.id.minus, R.id.multiply, R.id.divide};
 
         for (int id : idList) {
             View view = findViewById(id);
@@ -31,38 +27,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    protected void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putString("num1", num1.getText().toString());
+        state.putString("num2", num2.getText().toString());
+    }
+
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        num1.setText(state.getString("num1"));
+        num2.setText(state.getString("num2"));
+    }
+
     public void onClick(View view) {
-        Button button = (Button) view;
-        String buttonText = button.getText().toString();
-        boolean crashed = false;
-        switch (view.getId()) {
-            case R.id.button_pow:
-                expression.append(POW);
-                break;
-            case R.id.button_clear:
-                expression = new StringBuilder();
-                break;
-            case R.id.button_del:
-                if (expression.length() != 0) {
-                    expression.deleteCharAt(expression.length() - 1);
-                }
-                break;
-            case R.id.button_equals:
-                try {
-                    Calculator calculator = new SimpleCalculator();
-                    expression = new StringBuilder(Double.toString(calculator.calculate(expression.toString())));
-                } catch (Exception e) {
-                    crashed = true;
-                    screen.setText(CANNOT_CALCULATE);
-                    expression = new StringBuilder();
-                }
-                break;
-            default:
-                expression.append(buttonText);
-        }
-        if (!crashed) {
-            screen.setText(expression);
-            screen.setSelection(screen.getText().length());
+        try {
+            double a = Double.parseDouble(num1.getText().toString());
+            double b = Double.parseDouble(num2.getText().toString());
+            double res;
+            switch (view.getId()) {
+                case R.id.plus:
+                    res = a + b;
+                    break;
+                case R.id.minus:
+                    res = a - b;
+                    break;
+                case R.id.divide:
+                    res = a / b;
+                    break;
+                default:
+                    res = a * b;
+            }
+            DecimalFormat decimalFormat = new DecimalFormat();
+            decimalFormat.setMaximumFractionDigits(6);
+            decimalFormat.setMinimumFractionDigits(0);
+
+            num1.setText(Double.toString(res));
+            num2.setText("");
+        } catch (Exception e) {
+            num1.setText("");
+            num2.setText("");
+            Toast.makeText(getApplicationContext(), "Error occured!", Toast.LENGTH_SHORT).show();
         }
     }
 }
